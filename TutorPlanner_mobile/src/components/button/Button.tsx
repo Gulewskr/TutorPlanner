8a5f01a5ba6861682b149443819803a819301f69
grochowp <patgro3@wp.pl>
2024-09-14 16:45:26 +0200

    add custom Button component

--- TutorPlanner_mobile/src/components/button/Button.tsx
+++ TutorPlanner_mobile/src/components/button/Button.tsx
@@ -1,18 +1,96 @@
-import * as React from 'react';
-import { Button } from 'react-native';
-import styles from './Button.scss';
+import * as React from "react";
+import { Button, Image, Pressable, StyleSheet, Text, View } from "react-native";
+// import styles from "./Button.scss";
 
 interface ButtonProps {
-    secondary: boolean;
-    key: string;
-    title: string;
-    handleClick: () => void;
+  secondary: boolean;
+  icon: string;
+  // key: string;
+  title: string;
+  handleClick: () => void;
 }
 
-const CustomButton: React.FC<ButtonProps> = ({ key, title, handleClick }) => {
-    return <Button key={key} title={title} onPress={handleClick} />;
+const iconsMap: { [key: string]: any } = {
+  minus: require("../../assets/icons/minus.png"),
+  plus: require("../../assets/icons/plus.png"),
+  // check: require("../../assets/icons/check.png"),
 };
 
-CustomButton.displayName = 'Button';
+const CustomButton: React.FC<ButtonProps> = ({
+  // key,
+  title,
+  handleClick,
+  secondary,
+  icon,
+}) => {
+  const style = styles(secondary, !!title, icon);
+  return (
+    <View style={{ position: "relative" }}>
+      <Pressable style={style.button} onPress={handleClick}>
+        <View style={style.content}>
+          {icon && iconsMap[icon] && (
+            <Image source={iconsMap[icon]} style={style.icon} />
+          )}
+          <Text style={style.text}>{title}</Text>
+        </View>
+      </Pressable>
+      <View style={style.shadow} />
+    </View>
+  );
+};
+
+CustomButton.displayName = "Button";
 
 export default CustomButton;
+
+const styles = (secondary: boolean, isExpanded: boolean, icon?: string) =>
+  StyleSheet.create({
+    button: {
+      alignItems: "center",
+      justifyContent: "center",
+      height: 40,
+      borderRadius: 10,
+      width: isExpanded ? 200 : 40,
+      backgroundColor: secondary ? "#B0CFFF" : "#FFA9F1",
+      position: "relative",
+      shadowColor: "#000000",
+      elevation: 3,
+      zIndex: 10,
+      borderWidth: 1,
+      borderColor: "#070707",
+    },
+
+    content: {
+      flexDirection: "row",
+      alignItems: "center",
+      gap: 8,
+    },
+
+    icon: {
+      width: 20,
+      height: 20,
+      resizeMode: "contain",
+    },
+
+    text: {
+      fontSize: 16,
+      lineHeight: 21,
+      fontWeight: "bold",
+      letterSpacing: 0.25,
+      color: "#070707",
+      display: isExpanded ? "flex" : "none",
+    },
+
+    shadow: {
+      height: 40,
+      borderRadius: 10,
+      width: isExpanded ? 200 : 40,
+      position: "absolute",
+      top: 4,
+      left: 4,
+      backgroundColor: secondary ? "#002C9E" : "#9E0042",
+      borderWidth: 1,
+      borderColor: "#070707",
+      zIndex: 1,
+    },
+  });
--- TutorPlanner_mobile/src/screens/Home/Home.tsx
+++ TutorPlanner_mobile/src/screens/Home/Home.tsx
@@ -1,28 +1,45 @@
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-import styles from './Home.scss';
-import { StatusBar } from 'expo-status-bar';
+import * as React from "react";
+import { Button, View, Text } from "react-native";
+import styles from "./Home.scss";
+import { StatusBar } from "expo-status-bar";
+import CustomButton from "../../components/button/Button";
 
 export const Home: React.FC<{ navigation: any }> = ({ navigation }) => {
-    const users = ['OLA P', 'KAMIL S'];
+  const users = ["OLA P", "KAMIL S"];
 
-    return (
-        <View style={styles.container}>
-            <Text>😘</Text>
-            <Text>HOME SCREEN</Text>
-            <Text>
-                (to tylko tekst ale to już znak że zaczynamy działać :D)
-            </Text>
-            <StatusBar style="auto" />
-            {users.map((user, i) => (
-                <Button
-                    key={`user-${i}`}
-                    title={`Go to ${user} Page`}
-                    onPress={() =>
-                        navigation.navigate('Profile', { name: user })
-                    }
-                />
-            ))}
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>😘</Text>
+      <Text>HOME SCREEN</Text>
+      <Text>(to tylko tekst ale to już znak że zaczynamy działać :D)</Text>
+      <StatusBar style="auto" />
+      {users.map((user, i) => (
+        <Button
+          key={`user-${i}`}
+          title={`Go to ${user} Page`}
+          onPress={() => navigation.navigate("Profile", { name: user })}
+        />
+      ))}
+      <View style={{ gap: 10 }}>
+        <CustomButton
+          secondary={false}
+          icon="plus"
+          handleClick={() => 1}
+          title="Dodaj"
+        />
+        <CustomButton
+          secondary={false}
+          icon="minus"
+          handleClick={() => 1}
+          title=""
+        />
+        <CustomButton
+          secondary={true}
+          icon="minus"
+          handleClick={() => 1}
+          title="aaa"
+        />
+      </View>
+    </View>
+  );
 };
